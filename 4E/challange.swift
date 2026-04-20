f66786cbef23726947352d909bbe3e8ad96e8fa9
Zuza Jerzyńska <114587290+Zusza@users.noreply.github.com>
2026-04-20 12:43:15 +0100

    Update challange.swift

--- 4E/challange.swift
+++ 4E/challange.swift
@@ -1,8 +1,8 @@
 import Foundation
 //Zad.1 - Drugie największe w tablicy
-let l = [2, 5, 8, 1, 3]
-let sortowanie = l.sorted(by: >)
-let vicemax = sortowanie[1]
+var l = [2, 5, 8, 1, 3]
+var sortowanie = l.sorted(by: >)
+var vicemax = sortowanie[1]
 print(vicemax)
 
 //Zad.2 - Sprawdź, czy liczba jest pierwiastkowalna
@@ -18,3 +18,21 @@ func czyPierwiastek(n: Int) -> Bool{
 print(czyPierwiastek(n: 16))
 
 //Zad.3 - Najdłuższy ciąg rosnący w tablicy
+func najdluzszyCiag(tab: [Int]) -> [Int]{
+    var najciag: [Int] = []
+    var t: [Int] = [tab[0]]
+    for i in 1..<tab.count{
+        if tab[i] > tab[i-1]{
+            t.append(tab[i])
+        }
+        else{
+            if t.count > najciag.count{
+                najciag = t
+            }
+            t = [tab[i]]
+        }
+    }
+    return najciag
+}
+var tab = [1, 2, 3, 2, 5, 6, 7, 1]
+print(najdluzszyCiag(tab: tab))
